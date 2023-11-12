2c50c758cc9c96ce61bba5cd9d50e85219625911
Eric Bratu <gymnaztv@gmail.com>
2023-11-12 16:26:57 -0500

    score

---
+++ DevlUp Wargames/Assets/Scripts/scoredisplay.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoredisplay : MonoBehaviour
+{
+    public GameObject score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TMP_Text tmp = score.GetComponent<TMP_Text>();
+        tmp.text = "You Survived " + score + " Hallways...";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
