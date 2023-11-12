a0b707cfcacf52b1eded59e3f830fe00e40ce523
Liam McKenna <lmcmckenna@gmail.com>
2023-11-12 05:44:01 -0500

    MENU WORKING LETS GOOO
    IM GOING TO BED

---
+++ DevlUp Wargames/Assets/Scripts/ClickLoadScene.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickLoadScene : MonoBehaviour
+{
+    [Tooltip("The black screen transition that will be used")]
+    public Animation anim;
+
+    public int indexx;
+
+
+
+    public void LoadByIndex(int sceneIndex)
+    {
+        indexx = sceneIndex;
+        StartCoroutine(FadeOut());
+    }
+
+    public IEnumerator FadeOut()
+    {
+        anim.Play();
+        yield return new WaitForSeconds(1f); 
+        SceneManager.LoadScene(indexx);
+        Cursor.visible = false;
+        yield return new WaitForSeconds(1f);    
+                
+    
+    }
+}
---
+++ DevlUp Wargames/Assets/Scripts/GameSceneManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameSceneManager : MonoBehaviour
+{
+
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+    }
+
+    //This function should be called to other scripts so that way you have the transition working
+    public void LoadScene(int SceneIndex)
+    {
+        StartCoroutine(LoadAsyncScene(SceneIndex));
+    }
+
+    IEnumerator LoadAsyncScene(int SceneIndex)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(SceneIndex);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
